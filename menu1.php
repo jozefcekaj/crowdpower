f40eaaed3557dd9ad368fea0bd4199939153237f
eglmem <eglmem12@htl-shkoder.com>
2017-06-07 14:05:21 +0200

    Add files via upload

---
+++ menu1.php
@@ -0,0 +1,13 @@
+		<link rel="stylesheet" href="style.css" type="text/css" />	
+		<div id="page">
+		<div id="logo">
+			<h1><a href="index.php" id="logoLink">Crowd Power</a></h1>
+			<h3><a href="index.php" id="logoLink">Die Crowdfunding-Plattform f&uuml;r innovative Projekte</a></h3>
+			
+		</div>
+		<div id="nav">
+			<ul>
+				<li><a href="index.php">Home</a></li>			
+				<li><a href="logout.php">Logout</a></li>						
+			</ul>	
+		</div>
