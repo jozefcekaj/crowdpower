62558b69f15ef32e11d981993fd0e07038e5434f
jozefcekaj <jozcek12@htl-shkoder.com>
2017-06-07 14:01:56 +0200

    Add files via upload

---
+++ interneBereich.php
@@ -0,0 +1,47 @@
+
+<?php session_start(); ?>
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Crowd Power | Home </title>
+			<link rel="stylesheet" href="style.css" type="text/css" />	
+		</head>
+		<body>
+			
+			<?php require('menu1.php'); ?>
+			
+			<h2>Herzlich Willkommen!</h2>
+			<p>In diese Seite wird noch gearbeitet !!!</p>
+			<fieldset style="float:left">
+			<h2> Ausgangssituation</h2>
+			</br>
+				<p>Viele Businesspläne bleiben nur als eine Idee oder ein Schulprojekt, weil die
+				Schülern keine Gelegenheit, es zu realisieren haben. Auch die Diplomarbeiten werden auf
+				keine Online-Plattform gezeigt , und wegen alle diese, bleiben diese unrealisierbar. Nicht
+				alle Schüler in Shkodër haben die Möglichkeit zu lernen , wie man auf eine gute Business
+				Idee kommt und wie man einen Businessplan schreiben kann. </p> </br>
+			</fieldset>
+			<fieldset >
+			<h2> Beschreibung der Idee</h2></br>
+				<p>Jeder, der eine konkrete Projekt- oder Produktidee hat, soll sich auf unserer Crowdfunding-
+				Plattform anmelden. Wichtig ist, die richtige Website auszuwählen. So richtet sich zum
+				Beispiel unsere Website an Kreative und Erfinder. Ist die Registrierung erfolgreich, kann
+				das Projekt angelegt werden. Dabei sollten Kapitalsuchende auf eine möglichst präzise
+				Beschreibung achten. Das Projekt braucht außerdem einen festen Start- und Endpunkt und
+				die Angabe, welche Geldsummen zusammenkommen sollen. Und auch während der
+				Crowd-funding Phase sollte die Idee weiter beworben werden, um genügend Unterstützer
+				zu sammeln. Eine gute Idee alleine reicht nicht - je detaillierter der Businessplan ist, desto
+				größer sind die Chancen, möglichst viele Unterstützer zu gewinnen. Wird der angestrebte
+				Betrag in der vorgegebenen Zeit nicht erreicht, kommt das Projekt nicht zustände - die
+				Geldgeber erhalten ihren Einsatz zurück. Also, unsere Plattform wird alle Projekten , Ideen ,
+				Businesspläne und Diplomarbeiten auf unserer Plattform zeigen. Es wird eine Login -
+				Bereich sein, und alle Ideen werden nach bestimmte Kategorien sortiert. Auf unserer
+				Plattform werden auch Informationen mit Hilfe von Videos , Dokumenten und Beispiele
+				gezeigt , damit auch Leute, die nicht wissen , wie man auf eine gute Business Idee kommt ,
+				und wie man eine Businessplan schreiben, etwas lernen können. Auch Lehrern/innen und
+				die interessierten Menschen werden ein Account haben. Auf jeder Idee und Businessplan
+				werden die Telefonnummern des jeden Mitgliedern gezeigt , damit diese Leute miteinander
+				kontaktieren können. Wir werden auch mit YEA! kooperieren. YEA! ist ein Portal um die
+				Jugendliche zu helfen, ihren Ideen zu realisieren. </p></br>
+		</body>
+	</html>
